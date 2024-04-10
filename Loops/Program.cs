68fdaf269c23a9bbfac29df8e96b00c871931480
Sezin EROL <fenciszn@hotmail.com>
2024-04-10 20:55:22 +0300

    Day2 Task1 Commit5

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,10 +1,58 @@
-﻿for (int i = 1; i <=100; i=i+2)
-{
-    Console.WriteLine(i);
-}
-Console.WriteLine("Finished!");
+﻿//ForLoop();
+//ForLoop1();
+//WhileLoop();
+//DoWhileLoop();
+//ForEachLoop();
 
-for (int i = 100; i >= 0; i=i-2)
+
+
+
+
+//Extract methods
+static void ForLoop()
 {
-    Console.WriteLine(i);
+    for (int i = 1; i <= 100; i = i + 2)
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine("Finished!");
+
+}
+
+static void ForLoop1()
+{
+    for (int i = 100; i >= 0; i = i - 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+static void WhileLoop()
+{
+    int number = 100;
+    while (number >= 0)
+    {
+        Console.WriteLine(number);
+        number--;
+    }
+    Console.WriteLine("Now number is {0}", number);
+}
+
+static void DoWhileLoop()
+{
+    int number = 10;
+    do
+    {
+        Console.WriteLine(number);
+        number--;
+    } while (number >= 0);
+}
+
+static void ForEachLoop()
+{
+    string[] fruits = new string[3] { "Apple", "Banana", "Strawberry" };
+    foreach (var fruit in fruits)
+    {
+        Console.WriteLine(fruit);
+    }
 }
