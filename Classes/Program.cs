9a1de051f0b2166915c7f09684c0dd0b5bf6c7b1
Sezin EROL <fenciszn@hotmail.com>
2024-04-11 18:41:44 +0300

    RecapDemo

---
+++ Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using Classes;
+
+CustomerManager customerManager=new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+
+ProductManager productManager=new ProductManager();
+productManager.Add();
+productManager.Update();
+
+
+Customer customer = new Customer();
+customer.Id = 1;
+customer.FirstName = "Nizes";
+customer.LastName = "Lore";
+customer.City = "Artvin";
+
+Customer customer1 = new Customer
+{
+    Id=2, FirstName="Ayşe", LastName="Yılmaz", City="Ankara"
+};
+
+Console.WriteLine(customer1.FirstName);
+
+Console.ReadLine();
---
+++ RecapDemo1/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RecapDemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(483, 309);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+    }
+}
