3ed915551e9e872c5ac040d2e465f2cdcae11b70
Sezin EROL <fenciszn@hotmail.com>
2024-04-11 08:56:52 +0300

    IsPrimeNumber

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,6 +6,32 @@
 
 
 
+if (IsPrimeNumber(5))
+{
+    Console.WriteLine("This is a prime number");
+}
+else
+{
+    Console.WriteLine("This is a not prime number");
+}
+
+
+bool IsPrimeNumber(int number)
+{
+    bool result = true;
+    for (int i = 2; i < number - 1; i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            i = number;
+        }
+    }
+    return result;
+}
+
+
+
 
 
 //Extract methods
