6a20e15f7b24637581d428a1e770bf424294718f
Sezin EROL <fenciszn@hotmail.com>
2024-04-12 13:07:15 +0300

    Day2 Task1 Commit7

---
+++ Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿Person[] persons = new Person[2] { new Customer(), new Student() };  //Böyle de yazılabilir.
+
+//Böyle de yazılabilir. çünkü INTERFACE tek başına bir anlam ifade etmiyor ama CLASS larda Person bir anlam ifade ediyor. 
+Person[] persons1 = new Person[3] { new Customer { FirstName = "Nizes" }, new Student { FirstName = "Tuana" }, new Person { FirstName = "Demir" } };
+foreach (var person in persons1)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Department { get; set; }
+}
--- InterfacesDemo/Program.cs
+++ InterfacesDemo/Program.cs
@@ -1,1 +1,77 @@
-﻿
+﻿IWorker[] workers=new IWorker[3] {new Manager(),new Worker(),new Robot()};
+foreach (var worker in workers)
+{
+    worker.Work();
+}
+
+IEat[] eats=new IEat[2] {new Manager(), new Worker()};
+foreach (var eat in eats)
+{
+    eat.Eat();
+}
+
+
+
+Console.ReadLine();
+
+interface IWorker
+{
+    void Work();
+       
+}
+
+interface IEat
+{
+    void Eat();
+}
+
+interface ISalary
+{
+    void GetSalary();
+}
+
+
+
+class Manager : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        Console.WriteLine("Manager is eating!");
+    }
+
+    public void GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Work()
+    {
+        Console.WriteLine("I am a manager and I am working now");
+    }
+}
+
+class Worker : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        Console.WriteLine("Worker is eating!");
+    }
+
+    public void GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Work()
+    {
+        Console.WriteLine("I am a worker and I am working now");
+    }
+}
+
+class Robot : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("I am a robot and I am working now");
+    }
+}
