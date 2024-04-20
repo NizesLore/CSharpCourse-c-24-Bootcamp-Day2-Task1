2dcd364adb80b51d733ab01bf675fd0bcf64b754
Sezin EROL <fenciszn@hotmail.com>
2024-04-20 20:37:25 +0300

    Constructors Day2 Task2

---
+++ Constructors Day2 Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿Customer customer1 = new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };
+
+Customer customer3 = new Customer();
+customer3.Id = 3;
+customer3.FirstName = "Lale";
+customer3.LastName = "Zambak";
+customer3.City = "Bolu";
+
+
+Customer customer2 = new Customer(2, "Nizes", "Lore", "Artvin");
+
+class Customer()
+{
+    //default constructor
+
+    public Customer(int id, string firstName, string lastName, string city)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
