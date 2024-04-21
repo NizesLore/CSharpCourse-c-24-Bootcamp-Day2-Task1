719a10189740be02632c9322bc886525cd300218
Sezin EROL <fenciszn@hotmail.com>
2024-04-21 13:15:21 +0300

    Generics

--- Constructors Day2 Task2/Program.cs
+++ Constructors Day2 Task2/Program.cs
@@ -1,4 +1,4 @@
-﻿Customer customer1 = new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };
+﻿//Customer customer1 = new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };
 // {}icindekiler parametre değildir.{ den önce () yazılabilir de yazılmayabilir de.
 
 Customer customer3 = new Customer();//Bu yazım şekli yukarıdaki ile aynıdır.
@@ -8,25 +8,25 @@ customer3.LastName = "Zambak";
 customer3.City = "Bolu";
 
 
-Customer customer2 = new Customer(2, "Nizes", "Lore", "Artvin");
+//Customer customer2 = new Customer(2, "Nizes", "Lore", "Artvin");
 // Burada Parametreli Constructor çalışıyor.() icindekiler parametre
 
 class Customer()
 {
     //default constructor--> constructor without parameter
-    public Customer()
-    {
+    //public Customer()
+    //{
             
-    }
+    //}
 
     //constructor with parameter
-    public Customer(int id, string firstName, string lastName, string city)
-    {
-        Id = id;                 // Soldaki Id class ın prop u, sağdaki id () icindeki parametre.                     
-        FirstName = firstName;   // Id gelen id dir.FirstName gelen firstName dir.
-        LastName = lastName;     // Buyuk harf olanlar class prop ları, METHOD parametreleri camelCase yazılır.
-        City = city;             // Böyle buraya bunları yazarak CONSTRUCTOR da değerlerimizi set etmiş olduk.
-    }
+    //public Customer(int id, string firstName, string lastName, string city)
+    //{
+    //    Id = id;                 // Soldaki Id class ın prop u, sağdaki id () icindeki parametre.                     
+    //    FirstName = firstName;   // Id gelen id dir.FirstName gelen firstName dir.
+    //    LastName = lastName;     // Buyuk harf olanlar class prop ları, METHOD parametreleri camelCase yazılır.
+    //    City = city;             // Böyle buraya bunları yazarak CONSTRUCTOR da değerlerimizi set etmiş olduk.
+    //}
 
     public int Id { get; set; }
     public string FirstName { get; set; }
---
+++ Generics Day2 Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+List<string> sehirler = new List<string>();  //Oluştu
+sehirler.Add("Ankara");                      //Eleman ekledi
+Console.WriteLine(sehirler.Count);           //Eleman sayısı
+                           //Count burada bir prop, eğer method olsaydı yanında () olurdu, Count() yazılırdı.
+                           //Count bir property ve sadece {get;} yani sadece readonly
+
+MyList<string> sehirler2 = new MyList<string>();
+sehirler2.Add("Artvin");
+Console.WriteLine(sehirler2.Count);
+
+
+class MyList<T>       //Generic class
+{
+    T[] _array;
+    T[] _tempArray;   //Geçici array
+    public MyList()   //Ctor
+    {
+        _array = new T[0];   //O elemanlı bir array
+
+    }
+    public void Add(T item)
+    {
+        _tempArray = _array;               //_tempArray, _array in reference ını TUTUYOR
+        _array = new T[_array.Length + 1]; // Array lerin eleman sayıını 1 arttırınca NEW lememiz gerekiyor                  
+
+        //Ancak NEW lediğimizde bir önce array gidiyor, yeni array oluşuyor.O nedenle tempArray oluşturuyoruz.
+        //O elemanlı bir array old göre bizim onun eleman sayısını her eklememizde 1 arttırmamız gerek.
+        for (int i = 0; i < _tempArray.Length; i++) // i< Ne kadar dönecek? _tempArray kadar. _tempArray i aktaracağız
+        {                                        // _tempArray şu an burayı tututor,ABC elemanları var ya 3 kere dönecek
+            _array[i] = _tempArray[i];           //ABC 012 gibi,_tempArray kadar aktaracak
+        }                                        //O yüzden _array in i nci elemanı eşittir _tempArray in i nci elemanı
+
+        _array[_array.Length - 1] = item;        //_array in son elemanı nedir? _array.Length-1
+
+    }
+    public int Count
+    {
+        get { return _array.Length; }
+    }
+}
